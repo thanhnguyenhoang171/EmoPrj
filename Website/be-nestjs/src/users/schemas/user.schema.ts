2eabcd3d4ad87bcf19a3b71a720dafe15936828d
LAS_Chanh <thanhnguyenhoang171official@gmail.com>
2025-01-19 15:17:00 +0700

    update

--- Website/be-nestjs/src/users/schemas/user.schema.ts
+++ Website/be-nestjs/src/users/schemas/user.schema.ts
@@ -1,6 +1,6 @@
 
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { HydratedDocument } from 'mongoose';
+import mongoose, { HydratedDocument } from 'mongoose';
 
 export type UserDocument = HydratedDocument<User>;
 
@@ -14,6 +14,18 @@ export class User {
 
     @Prop()
     password: string;
+
+    @Prop()
+    role: string;
+
+    @Prop()
+    refreshToken: string;
+
+    @Prop({ type: Object })
+    updatedBy: {
+        _id: mongoose.Schema.Types.ObjectId;
+        name: string;
+    }
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
